833a5ea940906dce9041f2d9d0e3f111c5dc71da
Administrator <admin@local.host>
2013-10-22 11:27:24 +0300

    dsa

--- xcodeTest/AppDelegate.m
+++ xcodeTest/AppDelegate.m
@@ -16,7 +16,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
-    //hi
+    //hi hi hi
     return YES;
 }
 
