f6eb6e47ffbc8fabc3fee8e244dd87fd3a6e66ba
Administrator <admin@local.host>
2013-10-22 11:09:02 +0300

    Initial Commit

---
+++ xcodeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xcodeTest
+//
+//  Created by Ralph Esber on 10/22/13.
+//  Copyright (c) 2013 Ralph Esber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xcodeTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  xcodeTest
+//
+//  Created by Ralph Esber on 10/22/13.
+//  Copyright (c) 2013 Ralph Esber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
